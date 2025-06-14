8aaa18c73982a5eec3aa6a3e2bb692153af8db61
Mamun <psmfarhan@gmail.com>
2025-06-15 05:07:51 +0600

    people-page-done

--- js/people.js
+++ js/people.js
@@ -3,7 +3,7 @@ const dots = document.querySelectorAll(".dot");
 
 function moveToSlide(slideIndex) {
   const cardWidth = document.querySelector(".card").offsetWidth;
-  const offset = cardWidth * 3.3 * slideIndex;
+  const offset = cardWidth * 3.2 * slideIndex;
   track.style.transform = `translateX(-${offset}px)`;
 
   dots.forEach((dot) => dot.classList.remove("active"));
